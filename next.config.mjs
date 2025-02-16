5f03f80c0119a8d1393d55b35abb0f8cf1f94a79
Diana Tofan <diana_tofan@ymail.com>
2025-02-16 12:05:54 +0100

    Update base path

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,16 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const isProduction = process.env.NODE_ENV === 'production';
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
+  output: isProduction ? 'export' : 'standalone',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  basePath: isProduction ? '/nextjs-portfolio' : '',
 };
 
 export default nextConfig;
