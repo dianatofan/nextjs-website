373f5a596c4040ddbfacd25b4c84655daad10a39
Diana Tofan <diana_tofan@ymail.com>
2025-02-22 21:07:51 +0100

    Update video path

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -14,7 +14,7 @@ const VideoBackground = ({ children }) => {
         playsInline
         className="w-full h-full object-cover z-[-1]"
       >
-        <source src="/nextjs-website/images/background.mp4" type="video/mp4" />
+        <source src="/images/background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-10">
