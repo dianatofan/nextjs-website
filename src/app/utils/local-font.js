ead8d6c2367c4f41e4db32f0a79a326ba4ee5ab8
Diana Tofan <dianatofan@ITs-MacBook-Pro-3.local>
2025-07-09 11:45:36 +0200

    Fix errors

--- src/app/utils/local-font.js
+++ src/app/utils/local-font.js
@@ -29,13 +29,13 @@ export const metropolis = localFont({
   ],
 });
 
-export const nauryz = localFont({
-  src: [
-    {
-      path: '../fonts/Nauryz/Nauryz.ttf',
-      weight: '400',
-      style: 'normal',
-      format: 'truetype',
-    },
-  ],
-});
+// export const nauryz = localFont({
+//   src: [
+//     {
+//       path: '../fonts/Nauryz/Nauryz.ttf',
+//       weight: '400',
+//       style: 'normal',
+//       format: 'truetype',
+//     },
+//   ],
+// });
