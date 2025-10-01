9ca0380093c390f9baba86a2493814280339c91e
Diana Tofan <dianatofan@ITs-MBP-5.localdomain>
2025-10-01 14:43:43 +0200

    Update frontpage

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -5,7 +5,6 @@ import { a } from '@react-spring/web';
 import 'animate.css';
 import { metropolis, nauryz } from '@/app/utils/local-font';
 
-
 export default function Overlay({ fill }) {
   const [scrollTop, setScrollTop] = useState(0);
 
@@ -34,30 +33,17 @@ export default function Overlay({ fill }) {
 
   return (
     <div className="overlay animate__animated animate__fadeIn lg:w-1/2 lg:p-4 mx-8">
-      <a.span
+      <span
         className={`${metropolis.className} inline-block mt-4 mb-4 lg:text-3xl md:mb-6 md:text-base text-primary opacity-70 tracking-tighter font-semibold`}
-        style={{ color: fill }}
       >
         Hello! I&apos;m Diana Tofan
-        </a.span>
-        <a.div
-          className="inline-block rounded-2xl"
-          style={{
-            background: 'rgba(255,255,255,0.18)',
-            boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.12)',
-            backdropFilter: 'blur(16px) saturate(180%)',
-            WebkitBackdropFilter: 'blur(16px) saturate(180%)',
-            border: '1px solid rgba(255,255,255,0.25)',
-            padding: '0.5rem 1rem',
-          }}
-        >
-          <a.h1
-            className={`${nauryz.className} lg:text-8xl md:text-7xl text-6xl antialiased tracking-tighter lg:leading-headers text-primary font-medium lg:max-w-lg whitespace-nowrap`}
-            style={{ color: fill }}
-          >
-            UX Designer & Frontend Engineer
-          </a.h1>
-        </a.div>
+      </span>
+      <a.h1
+        className={`${nauryz.className} lg:text-8xl md:text-7xl text-6xl antialiased tracking-tighter lg:leading-headers text-primary font-medium lg:max-w-lg`}
+        style={{ color: fill }}
+      >
+        UX Designer & Frontend Engineer
+      </a.h1>
     </div>
   );
 }
